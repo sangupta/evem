5d4e564d9e5751b78a1a2ed6ac5d57dc0c0ffe5c
Sandeep Gupta <sandy.pec@gmail.com>
2022-08-24 22:03:49 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# evem
+
+`evem` is a simple `EventTarget` based event emitter. The usual method
+names have been shortened to `on`, `off`, `once` and `emit`. The original
+`emitter` instance is exported as well for those just in case scenarios.
+The only reason this package exists is so that code duplication may be
+prevented. Otherwise, I end up recreating these in every other library
+project.
+
+# Usage
+
+Use it simply as:
+
+```js
+import evem from 'evem'
+
+function handler(e) {
+    console.log('received an event with payload: ', e);
+}
+
+evem.on('my-custom-event', handler);
+evem.emit('my-custom-event'); // this prints the console message
+```
+
+# License
+
+[MIT](https://github.com/sangupta/evem/blob/main/LICENSE) License. Copyright (C) 2022, Sandeep Gupta.
---
+++ evem.ts
@@ -0,0 +1,74 @@
+/**
+ * evem - EventTarget Emitter
+ *
+ * MIT License.
+ * Copyright (c) 2022, Sandeep Gupta.
+ *
+ * Use of this source code is governed by a MIT style license
+ * that can be found in LICENSE file in the code repository.
+ */
+
+/**
+ * Internal EventTarget instance
+ */
+export const emitter = new EventTarget();
+
+/**
+ * Convenience function to add event handler.
+ * 
+ * @param name the event name
+ * 
+ * @param handler the event handler
+ */
+function on(name: string, handler: EventListenerOrEventListenerObject) {
+    emitter.addEventListener(name, handler);
+}
+
+/**
+ * Convenience function to add event handler that shall 
+ * only fire once.
+ * 
+ * @param name the event name
+ * 
+ * @param handler the event handler
+ */
+function once(name: string, handler: EventListenerOrEventListenerObject) {
+    emitter.addEventListener(name, handler, {
+        once: true
+    });
+}
+
+/**
+ * Convenience function to remove event handler.
+ * 
+ * @param name the event name
+ * 
+ * @param handler the event handler
+ */
+function off(name: string, handler: EventListenerOrEventListenerObject) {
+    emitter.removeEventListener(name, handler);
+}
+
+/**
+ * Convenience function to dispatch/raise/emit a new event
+ * with optional payload object.
+ * 
+ * @param name the event name
+ * 
+ * @param payload the optional payload to send along with event
+ */
+function emit(name: string, payload: object = {}) {
+    const event = new CustomEvent(name, {
+        detail: payload
+    });
+    emitter.dispatchEvent(event);
+}
+
+const evem = {
+    on: on,
+    off: off,
+    once: once,
+    emit: emit
+};
+
+export default evem;
